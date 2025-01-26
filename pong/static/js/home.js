c47c2f73809a342c3c6f88617b55d6d9e0346426
amyre <benhammouda.amir@gmail.com>
2025-01-26 22:20:05 +0100

    cleaning home.js + fixing injected scripts

--- pong/static/js/home.js
+++ pong/static/js/home.js
@@ -25,6 +25,17 @@ async function fetchView(path) {
 	}
 }
 
+// Execute scripts in the content
+function execScripts(content) {
+	const scripts = content.getElementsByTagName("script");
+	Array.from(scripts).forEach((script) => {
+		const newScript = document.createElement("script");
+		Array.from(script.attributes).forEach((attr) => newScript.setAttribute(attr.name, attr.value));
+		newScript.textContent = script.textContent;
+		script.parentNode.replaceChild(newScript, script);
+	});
+}
+
 // State management functions
 async function updateState(newState) {
 	Object.assign(AppState, newState);
@@ -39,28 +50,14 @@ async function updateState(newState) {
 }
 
 async function renderCurrentStep(step) {
+	console.log("func render");
 	const mainContent = document.body;
 	const view = views[step];
-
 	if (!view) return;
 
-	// Fetch and process the HTML content
-	const html = await fetchView(view.path);
-	console.log("HTML:", html);
-	mainContent.innerHTML = html.replace("${login}", AppState.user?.login).replace("${avatar}", AppState.user?.image.link);
-}
-
-async function renderCurrentStep(step) {
-	let AppState = JSON.parse(localStorage.getItem("pongAppState"));
-	console.log("AppState:", AppState);
-	const mainContent = document.body;
-	const view = views[step];
-
-	if (!view) return;
-
-	// Fetch and process the HTML content
 	const html = await fetchView(view.path);
 	mainContent.innerHTML = html.replace("${login}", AppState.user?.login).replace("${avatar}", AppState.user?.image.link);
+	execScripts(mainContent);
 }
 
 async function renderStep(step) {
@@ -75,12 +72,7 @@ async function renderStep(step) {
 	// Fetch and process the HTML content
 	const html = await fetchView(view.path);
 	mainContent.innerHTML = html.replace("${login}", AppState.user?.login).replace("${avatar}", AppState.user?.image.link);
-}
-
-async function renderHome() {
-	window.history.pushState({ step: 0 }, "", "/");
-	const html = await fetchView("/static/pages/home.html");
-	document.body.innerHTML = html;
+	execScripts(mainContent);
 }
 
 // Navigation functions
@@ -100,71 +92,12 @@ function previousStep() {
 	}
 }
 
-function goToStep(step) {
-	if (step >= 0 && step < Object.keys(views).length) {
-		updateState({ currentStep: step });
-	}
-}
-
-function logout() {
+async function logOut() {
 	localStorage.clear();
-	renderHome();
+	window.history.pushState({ step: 0 }, "", "/");
+	const html = await fetchView("/static/pages/notHome.html");
+	document.body.innerHTML = html;
 }
-
-// URL handling for the OAuth redirect
-
-// Event listener for when the page loads
-document.addEventListener("DOMContentLoaded", () => {
-	const urlParams = new URLSearchParams(window.location.search);
-	const code = urlParams.get("code");
-
-	const savedState = localStorage.getItem("pongAppState");
-	if (savedState) {
-		const parsedState = JSON.parse(savedState);
-		Object.assign(AppState, parsedState);
-		console.log("Restored state:", AppState.currentStep);
-		renderStep(AppState.currentStep);
-	} else {
-		if (code) {
-			localStorage.setItem("code", code);
-			localStorage.setItem("state", 1);
-
-			fetch("/api/token/", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ code: code }),
-			})
-				.then((response) => response.json())
-				.then((data) => {
-					localStorage.setItem("token", data.access_token);
-					localStorage.setItem("logged", "true");
-					console.log("Token data:", data.access_token);
-					url = "https://api.intra.42.fr/v2/me";
-					(headers = {
-						Authorization: `Bearer ${data.access_token}`,
-					}),
-						fetch(url, {
-							method: "GET",
-							headers: headers,
-						})
-							.then((response) => response.json())
-							.then((data) => {
-								console.log("User data:", data);
-								localStorage.setItem("user", JSON.stringify(data));
-								updateState({ currentStep: 1, user: data, gameOptions: null });
-							})
-							.catch((error) => {
-								console.error("Error:", error);
-							});
-				})
-				.catch((error) => {
-					console.error("Error:", error);
-				});
-		}
-	}
-});
 
 // Handle browser back/forward buttons
 window.addEventListener("popstate", (event) => {
