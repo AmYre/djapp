979b2b9dbd23c273a3ca472bb3ff6a0fc1d976da
amyre <benhammouda.amir@gmail.com>
2025-02-16 18:59:22 +0100

    update login api access

--- pong/api.py
+++ pong/api.py
@@ -11,7 +11,7 @@ def token(request):
     token_data = {
         'grant_type': 'authorization_code',
         'client_id': 'u-s4t2ud-278c6f5b974f198ff7770777621b6736535fe09144a049d6a79e2c37877665db',
-        'client_secret': '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',
+        'client_secret': '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',
         'code': code,
         'redirect_uri': 'http://localhost:8000'
     }
