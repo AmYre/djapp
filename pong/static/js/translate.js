088a9ae654c3eef14093e673a091850eaa7a14b1
rabouzia <54402804+rabouzia@users.noreply.github.com>
2025-03-22 02:03:10 +0100

    added different langage, but not everywhere

--- pong/static/js/translate.js
+++ pong/static/js/translate.js
@@ -1,14 +1,34 @@
+let defaultLang = localStorage.getItem("lang");
+if (!defaultLang)
+{
+    localStorage.setItem("lang", "en");
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	let select = document.getElementById("langSelector");
 
 	select.addEventListener('change', (event) => {
 		const selected = event.target.value;
 		const elements = document.querySelectorAll('[data-lang]');
-
-        // Update each element with the corresponding text
+        localStorage.setItem("lang", selected);
+            // Update each element with the corresponding text
         elements.forEach(element => {
             const key = element.getAttribute('data-lang');
             element.textContent = window.lang[selected][key];
         });
 	});
+
 });
+
+function applyTranslation(){
+    let lang = localStorage.getItem("lang");
+    console.log("HEEEy", lang);
+    // const lang = event.target.value; recupere la lang du localstorage
+    const elements = document.querySelectorAll('[data-lang]');
+        // Update each element with the corresponding text
+    elements.forEach(element => {
+        const key = element.getAttribute('data-lang');
+        element.textContent = window.lang[lang][key];
+    });
+}
--- pong/static/locales/lang.js
+++ pong/static/locales/lang.js
@@ -1,14 +1,113 @@
 window.lang = {
-	fr : {
-		homeTitle : "MONDE DE PONG",
-		subTitle : "Edition Transcendence",
+	en: {
+		flag : "🇬🇧",
+		homeTitle: "PONG WORLD",
+		subTitle: "Transcendence Edition",
+		modeTitle: "GAME MODE",
+
+		pspeed_title: "PADDLE SPEED",
+		pspeed_lvl1: "Painful",
+		pspeed_lvl2: "Slow",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Quick",
+		pspeed_lvl5: "Flash",
+		
+		pheight_title: "PADDLE HEIGHT",
+		pheight_lvl1: "Crazy",
+		pheight_lvl2: "Small",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Big",
+		pheight_lvl5: "Why?",
+
+		ballspeed_title: "BALL SPEED",
+		ballspeed_lvl1: "Boring",
+		ballspeed_lvl2: "Slow",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Quick",
+		ballspeed_lvl5: "Lightning",
+	
+		ballsize_title: "BALL SIZE",
+		ballsize_lvl1: "Quantic", 
+		ballsize_lvl2: "Small",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Big",
+		ballsize_lvl5: "Cosmic",
+
+		win: "Wins"
 	},
-	en : {
-		homeTitle : "PONG WORLD",
-		subTitle : "Transcendence Edition",
+	
+	fr: {
+		flag : "🇫🇷",
+
+		homeTitle: "MONDE DE PONG",
+		subTitle: "Édition Transcendence",
+		modeTitle: "MODE DE JEU",
+		
+		pspeed_title: "VITESSE PLANCHE",
+		pspeed_lvl1: "Douloureux",
+		pspeed_lvl2: "Lent",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Rapide",
+		pspeed_lvl5: "Éclair",
+		
+		pheight_title: "TAILLE PLANCHE",
+		pheight_lvl1: "Fou",
+		pheight_lvl2: "Petit",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Grand",
+		pheight_lvl5: "Pourquoi ?",
+	
+		ballspeed_title: "VITESSE BALLE",
+		ballspeed_lvl1: "Ennuyeux",
+		ballspeed_lvl2: "Lent",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Rapide",
+		ballspeed_lvl5: "Fulgurant",
+	
+		ballsize_title: "TAILLE BALLE",
+		ballsize_lvl1: "Quantique", 
+		ballsize_lvl2: "Petit",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Grand",
+		ballsize_lvl5: "Cosmique",
+
+		win: "Victoires"
 	},
-	es : {
-		homeTitle : "MUNDO DE PONG",
-		subTitle : "Edición de Trascendencia",
-	},
+	
+	es: {	
+		flag : "🇪🇸",
+		homeTitle: "MUNDO DE PONG",
+		subTitle: "Edición Trascendencia",
+		modeTitle: "MODO DE JUEGO",
+
+		pspeed_title: "VELOCIDAD DE LA PALETA",
+		pspeed_lvl1: "Doloroso",
+		pspeed_lvl2: "Lento",
+		pspeed_lvl3: "Normal",
+		pspeed_lvl4: "Rápido",
+		pspeed_lvl5: "Relámpago",
+		
+		pheight_title: "ALTURA DE LA PALETA",
+		pheight_lvl1: "Loco",
+		pheight_lvl2: "Pequeño",
+		pheight_lvl3: "Normal",
+		pheight_lvl4: "Grande",
+		pheight_lvl5: "¿Por qué?",
+	
+		ballspeed_title: "VELOCIDAD DE LA PELOTA",
+		ballspeed_lvl1: "Aburrido",
+		ballspeed_lvl2: "Lento",
+		ballspeed_lvl3: "Normal",
+		ballspeed_lvl4: "Rápido",
+		ballspeed_lvl5: "Veloz",
+	
+		ballsize_title: "TAMAÑO DE LA PELOTA",
+		ballsize_lvl1: "Cuántico", 
+		ballsize_lvl2: "Pequeño",
+		ballsize_lvl3: "Normal",
+		ballsize_lvl4: "Grande",
+		ballsize_lvl5: "Cósmico",
+
+		win: "Victorias"
+	}
 };
